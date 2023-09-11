0d472b2f4f6b585d35332c36d04d23471977d0b1
Ilya Denisenko <ilyadenisenko95@mail.ru>
2023-09-11 20:36:16 +0300

    working

---
+++ js/index.js
@@ -0,0 +1,30 @@
+document.getElementById("open-modal-btn").addEventListener("click" ,function() {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("close-my-modal-btn").addEventListener("click" ,function() {
+  document.getElementById("my-modal").classList.remove("open")
+})
+
+
+
+
+
+document.getElementById("open-modal-btn1").addEventListener("click" ,function() {
+  document.getElementById("my-modal1").classList.add("open")
+})
+
+document.getElementById("close-my-modal-btn1").addEventListener("click" ,function() {
+  document.getElementById("my-modal1").classList.remove("open")
+})
+
+
+
+
+document.getElementById("open-modal-btn2").addEventListener("click" ,function() {
+  document.getElementById("my-modal2").classList.add("open")
+})
+
+document.getElementById("close-my-modal-btn2").addEventListener("click" ,function() {
+  document.getElementById("my-modal2").classList.remove("open")
+})
