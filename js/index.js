97372d84f5a17eb3509c2fbf4f35f1148051c06e
Ilya Denisenko <ilyadenisenko95@mail.ru>
2023-09-12 13:50:15 +0300

    fixed problems

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-document.getElementById("open-modal-btn").addEventListener("click" ,function() {
+document.getElementById("open-modal-btn").addEventListener("click", function () {
   document.getElementById("my-modal").classList.add("open")
 })
 
-document.getElementById("close-my-modal-btn").addEventListener("click" ,function() {
+document.getElementById("close-my-modal-btn").addEventListener("click", function () {
   document.getElementById("my-modal").classList.remove("open")
 })
 
@@ -10,21 +10,21 @@ document.getElementById("close-my-modal-btn").addEventListener("click" ,function
 
 
 
-document.getElementById("open-modal-btn1").addEventListener("click" ,function() {
+document.getElementById("open-modal-btn1").addEventListener("click", function () {
   document.getElementById("my-modal1").classList.add("open")
 })
 
-document.getElementById("close-my-modal-btn1").addEventListener("click" ,function() {
+document.getElementById("close-my-modal-btn1").addEventListener("click", function () {
   document.getElementById("my-modal1").classList.remove("open")
 })
 
 
 
 
-document.getElementById("open-modal-btn2").addEventListener("click" ,function() {
+document.getElementById("open-modal-btn2").addEventListener("click", function () {
   document.getElementById("my-modal2").classList.add("open")
 })
 
-document.getElementById("close-my-modal-btn2").addEventListener("click" ,function() {
+document.getElementById("close-my-modal-btn2").addEventListener("click", function () {
   document.getElementById("my-modal2").classList.remove("open")
 })
